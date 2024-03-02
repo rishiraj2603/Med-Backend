23e919f7a1ead43d4461f4632c1185007d17fbaf
Rishi Raj <rishi@Rishis-MacBook-Air.local>
2024-03-02 12:49:27 +0530

    cors error solved

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.connect(MONGO_DB_URL).then(() => {
   console.log("mongoDB running");
 });
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(medicineRoutes);
 app.use(userRoutes);
 
