6b55609df5b841c425ae33b3e1eb2bcfcec0f159
Rishi Raj <rishi@Rishis-MacBook-Air.local>
2024-05-23 16:02:58 +0530

    ocr value change

--- routes/ocr.js
+++ routes/ocr.js
@@ -1,8 +1,12 @@
 //dependencies
+require("dotenv").config();
 const router = require("express").Router();
 const fs = require("fs");
 const multer = require("multer");
 const { createWorker } = require("tesseract.js");
+const presetValue = process.env.PRESET_NAME;
+const cloudApiKey = process.env.CLOUD_API_KEY;
+const cloudName = process.env.CLOUD_NAME;
 
 //storage
 // const storage = multer.diskStorage({
@@ -26,11 +30,11 @@ router.post("/upload", (req, res) => {
         const ret = await worker.recognize(data);
         const textData = ret.data.text;
         //console.log(ret.data.text);
-        res.status(200).json(textData);
         await worker.terminate();
       })();
     });
   });
+  res.status(200).json({ textData, presetValue, cloudApiKey, cloudName });
 });
 
 module.exports = router;
